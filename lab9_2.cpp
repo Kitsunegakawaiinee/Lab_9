4d6c22b512573d1224e602596c7dc3f38df9bad4
SKOT <Korawit_pi@cmu.ac.th>
2022-12-23 17:09:42 +0700

    ข้อ_4

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -3,34 +3,51 @@
 #include<string>
 using namespace std;
 
-//[Missing Code 1] Write definition of the function findGrade() here.
+#define elif else if
 
-int main(){
-	//Input the number of students
-	int N,i = 0;
+char findGrade(float Score)
+{
+	char Grade;
+	bool A,B,C,D,F;
+	A = ( Score > 90 );
+	B = ( Score > 75 && Score <= 90);
+	C = ( Score > 60 && Score <= 75);
+	D = ( Score > 45 && Score <= 60);
+	F = ( Score <= 45);
+
+	if(A) Grade = 'A';
+	elif (B) Grade = 'B';
+	elif (C) Grade = 'C';
+	elif (D) Grade = 'D';
+	elif (F) Grade = 'F';
+
+	return Grade ;
+}
+
+
+int main()
+{
+	int N;
 	cout << "Enter the number of students: ";
 	cin >> N;
+	cin.ignore();
 	string name[N];
 	float score[N];	
 	
-	//Store names and scores of students into an array 
-	while(i < N){
+	for(int i = 0; i < N ;i++){
 		cout << "Name of student " << i+1 << ": ";
-		cin.ignore();
-		//[Missing Code 2] Get name of the i-th students that may include whitespace.
+		getline(cin,name[i]);
 		cout << "Score of student " << i+1 << ": ";
-		//[Missing Code 3] Get score of the i-th students.
-		i++;
+		cin >> score[i];
+		cin.ignore();
 	}
 	
-	//Print names scores and grades
-	i = 0;
 	cout << "---------------------------------------------\n";
 	cout << setw(25) << "Name" << setw(8) << "Score" << setw(8) << "Grade" << "\n";
 	cout << "---------------------------------------------\n";
-	while(i < N){
+	for(int i =0; i < N; i++)
+	{
 		cout << setw(25) << name[i] << setw(8) << score[i] << setw(8) << findGrade(score[i]) << "\n";
-		i++;
 	} 
 	cout << "---------------------------------------------\n";
 

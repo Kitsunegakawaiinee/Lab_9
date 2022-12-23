a89a432fc77f01b8df42cbb32f687e64cb680a74
SKOT <Korawit_pi@cmu.ac.th>
2022-12-23 15:48:17 +0700

    ข้อ_1

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
 using namespace std;
 
+#define EHE else if
+#define NANDAYO if
+#define NONONO else
+
 int main()
 {
   char rank;
   cout << "Input your rank: ";
   cin >> rank;
-  switch(rank){
-    case 'S': cout << "You have received Super Ultra Rare Unit!!!\n";
-    case 'A': cout << "You have received 5 gems.\n";
-    case 'B': cout << "You have received 1 gems.\n";
-    case 'C': cout << "You have received 2000 coins.\n";
-    case 'D': cout << "You have received very KAK items.\n";
-  }
+
+  bool s,a,b,c,FALSE;
+  s = (rank  == 'S' || rank == 's');
+  a = (rank == 'A' || rank == 'a');
+  b = (rank == 'B' || rank == 'b');
+  c = (rank == 'C' || rank == 'c');
+  FALSE = (rank == 'D' || rank == 'd');
+
+  NANDAYO (s) cout << "You have received Super Ultra Rare Unit!!!\n" << "You have received 5 gems.\n" << "You have received 1 gems.\n" << "You have received 2000 coins.\n"<< "You have received very KAK items.\n";
+  EHE (a)  cout << "You have received 5 gems.\n" << "You have received 1 gems.\n" << "You have received 2000 coins.\n"<< "You have received very KAK items.\n";
+  EHE (b) cout << "You have received 1 gems.\n"<< "You have received 2000 coins.\n"<< "You have received very KAK items.\n";
+  EHE (c) cout << "You have received 2000 coins.\n" << "You have received very KAK items.\n";
+  EHE (FALSE) cout << "You have received very KAK items.\n";
+  NONONO return 0;
+
   return 0;
 }
